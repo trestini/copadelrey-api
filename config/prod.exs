3a572b798c918fae9d38b6ab55d73aba96b9434f
Rafael Trestini <rafael.trestini@ifood.com.br>
2019-09-21 20:46:22 -0300

    heroku configuration

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :copadelrey_api, CupWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "copadelrey-api", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
 
 # Do not print debug messages in production
 config :logger, level: :info
